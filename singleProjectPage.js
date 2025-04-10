da9abdff0b1dad75cf70666c9351b3d459e172fc
shaked <shakedblank@gmail.com>
2025-04-10 20:42:48 +0300

    woroked on the single project page

--- singleProjectPage.js
+++ singleProjectPage.js
@@ -13,5 +13,19 @@ const projectObject= json.Projects[projectNum];
 
 const mainTitle = document.querySelector("#Project-title");
 const description = document.querySelector("#Project-description");
+const imageOne = document.querySelector("#image-one");
+const imageTwo = document.querySelector("#image-two");
+const imageThree = document.querySelector("#image-three");
+const imageFour = document.querySelector("#image-four");
+const imageFive = document.querySelector("#image-five");
+const imageSix = document.querySelector("#image-six");
+
 mainTitle.textContent= projectObject.innerTitle;
 description.textContent= projectObject.innerP;
+imageOne.src=projectObject.image1;
+imageTwo.src=projectObject.image2;
+imageThree.src=projectObject.smallImage1;
+imageFour.src=projectObject.smallImage2;
+imageFive.src=projectObject.smallImage3;
+imageSix.src=projectObject.smallImage4;
+
